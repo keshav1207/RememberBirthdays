2f650e055def4077b0d827c64fabfd18f77c032f
Keshav Callychurn <keshav0799@gmail.com>
2025-06-01 21:48:22 -0400

    Add getters and setters for person class

--- src/main/java/com/example/RememberBirthdays/Model/Person.java
+++ src/main/java/com/example/RememberBirthdays/Model/Person.java
@@ -3,6 +3,7 @@ package com.example.RememberBirthdays.Model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 import java.time.LocalDate;
 
@@ -11,12 +12,37 @@ import java.time.LocalDate;
 public class Person{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long Id;
+    private Long id;
 
     private String firstName;
     private String lastName;
     private LocalDate birthDate;
 
+    public Long getId() {
+        return id;
+    }
+    
+    public String getFirstName(){
+        return this.firstName;
+    }
 
+    public String getLastName(){
+        return this.lastName;
+    }
 
+    public LocalDate getBirthDate(){
+        return this.birthDate;
+    }
+
+    public void setFirstName(String firstname){
+        this.firstName = firstname;
+    }
+
+    public void setLastName(String lastname){
+        this.lastName = lastname;
+    }
+
+    public void setBirthDate(LocalDate date){
+         this.birthDate = date;
+    }
 }
