d9c6dac88ae25bbc360e4fa0deb300992e93da88
Keshav Callychurn <keshav0799@gmail.com>
2025-10-05 17:59:34 -0400

    Add additional code to fix cors error when using authorization token

--- src/main/java/com/example/RememberBirthdays/Config/SecurityConfig.java
+++ src/main/java/com/example/RememberBirthdays/Config/SecurityConfig.java
@@ -5,6 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -15,7 +21,21 @@ public class SecurityConfig {
                     )
                     .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults())
                     );
+                    http.cors(cors -> cors.configurationSource(corsConfiguration()));
                     return http.build();
+        }
+
+        @Bean
+        public CorsConfigurationSource corsConfiguration(){
+                CorsConfiguration configuration = new CorsConfiguration();
+                configuration.setAllowedOrigins(List.of("http://localhost:3000/"));
+                configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT","DELETE"));
+                configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+                configuration.setAllowCredentials((true));
+
+                UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+                source.registerCorsConfiguration("/api/**",configuration);
+                return source;
         }
 
 
