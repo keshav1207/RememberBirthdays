1a44826097676d9c1150063e7d748171e9bd5606
Keshav Callychurn <keshav0799@gmail.com>
2025-10-26 19:56:06 -0400

    Create KeycloakConfig

---
+++ src/main/java/com/example/RememberBirthdays/Config/KeycloakConfig.java
@@ -0,0 +1,37 @@
+package com.example.RememberBirthdays.Config;
+
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KeycloakConfig {
+
+    @Value("${keycloak.server-url}")
+    private String serverUrl;
+
+    @Value("${keycloak.realm}")
+    private String realm;
+
+    @Value("${keycloak.username}")
+    private String username;
+
+    @Value("${keycloak.password}")
+    private String password;
+
+    @Value("${keycloak.client-id}")
+    private String clientId;
+
+    @Bean
+    public Keycloak keycloak() {
+        return KeycloakBuilder.builder()
+                .serverUrl(serverUrl)
+                .realm(realm)
+                .username(username)
+                .password(password)
+                .clientId(clientId)
+                .build();
+    }
+}
