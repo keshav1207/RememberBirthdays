60096c027853fc6806f3bb37c4c109f99ddf52ef
Keshav Callychurn <keshav0799@gmail.com>
2025-11-13 21:36:34 -0500

    Update  delete and update method in personController to allow Admin to call these API

--- src/main/java/com/example/RememberBirthdays/Controller/PersonController.java
+++ src/main/java/com/example/RememberBirthdays/Controller/PersonController.java
@@ -9,7 +9,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
@@ -40,9 +44,17 @@ public class PersonController {
     public ResponseEntity<?> deletePerson(@PathVariable Long id, @org.springframework.security.core.annotation.AuthenticationPrincipal Jwt jwt){
         User user  = userservice.findOrCreateUser(jwt);
 
+        Map<String, Object> realmAccess = jwt.getClaim("realm_access");
+        Set<String> realmRoles = new HashSet<>();
+        if (realmAccess != null && realmAccess.containsKey("roles")) {
+            realmRoles.addAll((List<String>) realmAccess.get("roles"));
+        }
+
+        boolean isAdmin = realmRoles.contains("Admin");
+
         return repository.findById(id)
                 .map(person -> {
-                    if(!person.getUser().getUserId().equals(user.getUserId())){
+                    if(!(person.getUser().getUserId().equals(user.getUserId()) ) && !isAdmin){
                         return ResponseEntity.status(403).body("Authorization required to delete this birthday");
                     }
 
@@ -59,11 +71,18 @@ public class PersonController {
     @PutMapping("/{id}")
     public ResponseEntity<?> updatePerson(@PathVariable Long id, @RequestBody @Valid  Person updatedPerson, @org.springframework.security.core.annotation.AuthenticationPrincipal Jwt jwt){
         User user = userservice.findOrCreateUser(jwt);
+        Map<String, Object> realmAccess = jwt.getClaim("realm_access");
+        Set<String> realmRoles = new HashSet<>();
+        if (realmAccess != null && realmAccess.containsKey("roles")) {
+            realmRoles.addAll((List<String>) realmAccess.get("roles"));
+        }
 
+        boolean isAdmin = realmRoles.contains("Admin");
         return repository.findById(id)
                 .map(person -> {
 
-                    if(!person.getUser().getUserId().equals(user.getUserId())){
+                    if(!(person.getUser().getUserId().equals(user.getUserId()) ) && !isAdmin){
+                        System.out.println("isAdmin = " + isAdmin);
                         return ResponseEntity.status(403).body("Authorization required to edit this birthday");
                     }
 
