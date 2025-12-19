4184e2acfbb300f417eca7da41f5b3833cda5c56
Keshav Callychurn <keshav0799@gmail.com>
2025-12-18 20:07:08 -0500

    Add new method to query today's birthday

--- src/main/java/com/example/RememberBirthdays/Repository/PersonRepository.java
+++ src/main/java/com/example/RememberBirthdays/Repository/PersonRepository.java
@@ -1,14 +1,23 @@
 package com.example.RememberBirthdays.Repository;
+
 import com.example.RememberBirthdays.Model.Person;
 import com.example.RememberBirthdays.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
+@Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
+
     List<Person> findAllByUser(User user);
 
+
     @Query("SELECT p FROM Person p WHERE EXTRACT(MONTH FROM p.birthDate) = :month AND EXTRACT(DAY FROM p.birthDate) = :day")
-    List<Person> findByBirthdayMonthAndDay(@Param("month") int month, @Param("day") int day);
+    List<Person> findByBirthdayMonthAndDay(int month, int day);
+
+
+    @Query("SELECT p FROM Person p WHERE FUNCTION('DATE', p.birthDate) = CURRENT_DATE")
+    List<Person> findTodayBirthdays();
 }
