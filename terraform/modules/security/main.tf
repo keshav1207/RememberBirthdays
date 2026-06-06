df888496a290c664933291f80e681d03b7950f37
keshav <keshav0799@gmail.com>
2026-06-06 19:21:14 -0400

    Add git pull to CD pipeline

--- terraform/modules/security/main.tf
+++ terraform/modules/security/main.tf
@@ -41,6 +41,14 @@ resource "aws_security_group" "ec2" {
     ]
   }
 
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Grafana"
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
